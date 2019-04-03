9ba3f613e4f7d2bb3705cac1f6665028c8972bac
Gerald Fuchs <gerald@fuchsclan.de>
2019-04-03 19:16:39 +0200

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { HomeComponent } from "./home/home.component";
+import { ProcesslistComponent } from "./processlist/processlist.component";
+import { StartProcessComponent } from "./start-process/start-process.component";
+import { TasklistComponent } from "./tasklist/tasklist.component";
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "processlist", component: ProcesslistComponent },
+  {
+    path: "startprocess/:processdefinitionkey",
+    component: StartProcessComponent
+  },
+  { path: "tasklist", component: TasklistComponent },
+  { path: "tasklist/:id", component: TasklistComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { SuiModule } from "ng2-semantic-ui";
+
+import { CamundaRestService } from "./camunda-rest.service";
+import { ProcesslistComponent } from "./processlist/processlist.component";
+import { TasklistComponent } from "./tasklist/tasklist.component";
+import { HomeComponent } from "./home/home.component";
+import { StartProcessComponent } from "./start-process/start-process.component";
+import { GenericForm } from "./generic-form.component";
+import { MyAddonModule } from "./forms/myprocess/myAddon.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProcesslistComponent,
+    TasklistComponent,
+    HomeComponent,
+    StartProcessComponent,
+    GenericForm
   ],
   imports: [
+    FormsModule,
+    SuiModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MyAddonModule
   ],
-  providers: [],
+  providers: [CamundaRestService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
